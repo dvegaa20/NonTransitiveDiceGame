48e6f78cb9b61482a4a93264e09e326a7b0d1ad9
dvegaa20 <diego.vega.camacho@gmail.com>
2025-01-21 09:42:26 -0600

    feat(main): added visuals.

--- dice_game.py
+++ dice_game.py
@@ -10,16 +10,14 @@ def main():
         dice = DiceParser.parse(args)
         print("Dice parsed successfully: ", dice)
 
-        print("Probability table:")
-        ProbabilityTable(dice)
+        print("\n=== Probability Table ===")
+        ProbabilityTable.generate_probability_table(dice)
 
-        print("Starting game")
+        print("\n=== Starting the Game ===")
         game = Game(dice)
         game.play()
     except ValueError as e:
         print("Error: ", e)
-        print("Usage: python dice_game.py <die1> <die2> <die3>")
-        print("Example: python dice_game.py 2,2,4,4,9,9 6,8,1,1,8,6 7,5,3,7,5,3")
 
 
 if __name__ == "__main__":
